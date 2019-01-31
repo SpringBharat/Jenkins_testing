cad64c24af7b0612168f45f3757bebb4c2c47321
bharat <bharat.patel@computronics.in>
2019-01-31 16:53:19 +0530

    Second Project

---
+++ Jenkins-Start/src/main/java/com/example/demo/JenkinsStartApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsStartApplication.class, args);
+	}
+
+}
+
