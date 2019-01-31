93893c4f8e33f759ff4c4f00b83172921abddf1b
bharat <bharat.patel@computronics.in>
2019-01-31 16:05:25 +0530

    First Commit

---
+++ Jenkins-Testing/src/main/java/com/bharat/Main.java
@@ -0,0 +1,29 @@
+/**
+/**
+ * 
+ */
+package com.bharat;
+
+/**
+ * @author admin
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		System.out.println("Main.Main()");
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Main.main()");
+	}
+
+}
